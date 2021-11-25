d0f3f601701df8ce5fbbb2d57a91b8e18eb08609
FelipeSantos019 <luisgomessantos05@gmail.com>
2021-11-25 19:51:54 +0000

    ProjetoPW-Parte01

---
+++ projeto/src/views/contents/Inicio.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Inicio = props => (
+    <div>
+        <h1>Bem Vindo a Pagina inicial</h1>
+        <br />
+        <h2>Aqui, contaremos tudo sobre a carreira do ator Cillian Murphy</h2> 
+    </div>
+)
+
+export default Inicio
---
+++ projeto/src/views/contents/Introducao.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Introducao = props => (
+    <div>
+        <h1>Introdução do assunto</h1>
+        <h2>O Cillian Murphy é um ator conhecido por suas colaborações
+              com o diretor Christopher Nolan, interpretando o Espantalho na trilogia de The Dark Knight
+               (2005-2012) e aparecendo no thriller de ação e ficção científica Inception (2010) e no
+                drama de guerra Dunkirk (2017).</h2>
+    </div>
+)
+export default Introducao
